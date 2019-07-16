2c68bb064712a7568560ad61c894098bc44b771b
Mark Lobodzinski <mark@lunarg.com>
2019-07-16 15:53:01 -0600

    corechecks: Add IndexBuffers to CB's object_bindings
    Change-Id: I59ee3186cc1bae8422b8db008f66c5a2cf4f314c

--- layers/core_validation.cpp
+++ layers/core_validation.cpp
@@ -6952,13 +6952,16 @@ bool CoreChecks::PreCallValidateCmdBindIndexBuffer(VkCommandBuffer commandBuffer
 void CoreChecks::PreCallRecordCmdBindIndexBuffer(VkCommandBuffer commandBuffer, VkBuffer buffer, VkDeviceSize offset,
                                                  VkIndexType indexType) {
     auto buffer_state = GetBufferState(buffer);
-    auto cb_node = GetCBState(commandBuffer);
+    auto cb_state = GetCBState(commandBuffer);
 
-    cb_node->status |= CBSTATUS_INDEX_BUFFER_BOUND;
-    cb_node->index_buffer_binding.buffer = buffer;
-    cb_node->index_buffer_binding.size = buffer_state->createInfo.size;
-    cb_node->index_buffer_binding.offset = offset;
-    cb_node->index_buffer_binding.index_type = indexType;
+    cb_state->status |= CBSTATUS_INDEX_BUFFER_BOUND;
+    cb_state->index_buffer_binding.buffer = buffer;
+    cb_state->index_buffer_binding.size = buffer_state->createInfo.size;
+    cb_state->index_buffer_binding.offset = offset;
+    cb_state->index_buffer_binding.index_type = indexType;
+    // Add binding for this index buffer to this commandbuffer
+    cb_state->object_bindings.emplace(buffer, kVulkanObjectTypeBuffer);
+
 }
 
 bool CoreChecks::PreCallValidateCmdBindVertexBuffers(VkCommandBuffer commandBuffer, uint32_t firstBinding, uint32_t bindingCount,
